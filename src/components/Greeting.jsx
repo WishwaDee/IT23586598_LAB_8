d6343ac8131fe25f4002f6deaa2e06e4e8aab6f8
Wishwa Dilshan <wishwadilshan628@gmail.com>
2026-05-01 14:21:31 +0530

    Exercise 01

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,10 @@
+function Greeting() {
+  return (
+    <div className="greeting-container">
+      <h1>Hello World!</h1>
+      <p>Welcome to my first React app.</p>
+    </div>
+  );
+}
+
+export default Greeting;
